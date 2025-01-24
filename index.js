b55353ef717051a3d3744f92e9eecf51c041de85
qkrdydqls999 <162669259+qkrdydqls999@users.noreply.github.com>
2025-01-24 18:09:07 +0900

    please do

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 async function makeIssue() {
     const token = process.env.GH_TOKEN; // 주의! 일치해야함 (env와 일치해야함)
     // 요고 1
-    const OWNER = "qus0in"; // 여러분들의 github 계정 이름
+    const OWNER = "qkrdydqls999"; // 여러분들의 github 계정 이름
     // 요고 2
-    const REPO = "github_power_actions"; // 여러분들의 현재 리포지터리 이름
+    const REPO = "github_power_actions_To_ME"; // 여러분들의 현재 리포지터리 이름
     const response = await fetch(`https://api.github.com/repos/${OWNER}/${REPO}/issues`, {
         method: 'POST',
         headers: {
             Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify({
-            title: "행운의 숫자", // 요고 3
-            body: `${Math.floor(Math.random() * 100) + 1}`, // 요고 4
+            title: "오늘의 숫자는 뭘까~요옹?", // 요고 3
+            body: `${Math.floor(Math.random() * 10) + 1}`, // 요고 4
         })
     });
     if (response.ok) {
