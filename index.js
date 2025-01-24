68193fe8cf0d5158bd378c66551f9c347ad7d6ca
Young-in Byeon <50694786+qus0in@users.noreply.github.com>
2025-01-24 17:41:18 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+async function makeIssue() {
+    const token = process.env.GITHUB_TOKEN;
+    // 요고 1
+    const OWNER = "qus0in"; // 여러분들의 github 계정 이름
+    // 요고 2
+    const REPO = "github_power_actions"; // 여러분들의 현재 리포지터리 이름
+    const response = await fetch(`https://api.github.com/repos/${OWNER}/${REPO}/issues`, {
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+            title: "행운의 숫자", // 요고 3
+            body: `${Math.floor(Math.random() * 100) + 1}`, // 요고 4
+        })
+    });
+    if (response.ok) {
+        console.log("성공");
+    } else {
+        console.log("실패");
+    }
+}
+
+makeIssue();
